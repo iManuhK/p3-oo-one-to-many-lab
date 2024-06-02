9cb9f972bac332703f33f24cf18d5346b5901354
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-06-02 17:48:31 +0300

    additional commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,33 +1,32 @@
 class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     all = []
-    def __init__(self,pet_type):
-        self.pet_type = pet_type
-        self._owner = None
-        if self.pet_type not in Pet.PET_TYPES:
-            raise Exception(f"{self.pet_type} is not a valid pet type")
-       
-    @property
-    def owner(self):
-        return self._owner
-    
-    @owner.setter
-    def owner(self,owner):
-        if not isinstance(owner, Owner):
-            raise TypeError("owner must be an instance of Owner class")
-        self._owner = owner
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        else:
+            self.pet_type = pet_type
+        self.name = name
+        self.owner = owner
+        self.all.append(self)
+        if owner:
+            owner.add_pet(self)
 
 class Owner:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
+        self.pets_list = []
 
     def pets(self):
-        return [pet for pet in Pet.all if pet.owner == self]
-    def add_pet(self,pet):
-        if isinstance(pet,Pet):
-            pet.owner = self
-            self.pets.append(pet)
-        else:
-            raise Exception(f"{pet} is not a Pet")
+        return self.pets_list
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Invalid pet object")
+        self.pets_list.append(pet)
+
     def get_sorted_pets(self):
-        return sorted(self.pets(), key=lambda pet: pet.name)
+        sorted_pets = sorted(self.pets_list, key=lambda pet: pet.name)
+        return sorted_pets
+
